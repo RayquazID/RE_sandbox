264a5073add5a0d9f625bebe400628b99b3efdac
rayquazid <rayquazid@protonmail.com>
2021-09-02 14:48:53 +0200

    update pathes

--- setup_sandbox.ps1
+++ setup_sandbox.ps1
@@ -1,16 +1,21 @@
 function InitSetup {
     param (
     )
-    $BasePath = "C:\Setup"
+    $BasePath = "C:/Setup"
+    
+    New-Item -Path "$BasePath" -Type Directory
+    New-Item -Path "$BasePath/Installer/" -Type Directory
+    New-Item -Path "$BasePath/Tools/" -Type Directory
     Set-Location $BasePath
 }
 function DownloadProcMon {
     param (
     )
+    New-Item -Path "$BasePath/Tools/ProcMon" -Type Directory
     $urlProcMon = "https://download.sysinternals.com/files/ProcessMonitor.zip"
     $FileName = "ProcessMonitor.zip"
-    if ( -Not (Test-Path $FileName)) {
-        Invoke-WebRequest -Uri $urlProcMon -OutFile $FileName
+    if ( -Not (Test-Path "$BasePath\$FileName")) {
+        Invoke-WebRequest -Uri $urlProcMon -OutFile $BasePath$FileName
         Expand-Archive $FileName -DestinationPath "$BasePath/Tools/ProcMon/" -Force
         Remove-Item $FileName
     }
@@ -22,6 +27,7 @@ function DownloadProcMon {
 function DownloadWireshark {
     param (
     )
+    New-Item -Path "$BasePath/Installer/Wireshark" -Type Directory
     $urlWireShark = "https://1.eu.dl.wireshark.org/win64/Wireshark-win64-3.4.8.exe"
     $FileName = "Wireshark-win64-3.4.8.exe"
     if ( -Not (Test-Path "$BasePath/Installer/Wireshark/$FileName")) {
@@ -32,7 +38,7 @@ function DownloadWireshark {
 function DownloadMozilla {
     param (
     )
-    #$urlMozilla = "https://www.mozilla.org/de/firefox/download/thanks/"
+    New-Item -Path "$BasePath/Installer/Mozilla" -Type Directory
     $urlMozilla = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/91.0.2/win64/de/Firefox%20Setup%2091.0.2.msi"
     $FileName = "Firefox Setup 91.0.2.msi"
     if ( -Not (Test-Path "$BasePath/Installer/Mozilla/$FileName")) {
@@ -42,6 +48,7 @@ function DownloadMozilla {
 function DownloadBurp {
     param (
     )
+    New-Item -Path "$BasePath/Installer/Burp" -Type Directory
     $urlBurp = "https://portswigger.net/burp/releases/download?product=community&version=2021.8.2&type=WindowsX64"
     $FileName = "burpsuite_community_windows-x64_v2021_8_2.exe"
     if ( -Not (Test-Path "$BasePath/Installer/Mozilla/$FileName")) {
@@ -51,10 +58,11 @@ function DownloadBurp {
 function DownloadVSCode {
     param (
     )
+    New-Item -Path "$BasePath/Installer/vscode" -Type Directory
     $urlVSCode = "https://az764295.vo.msecnd.net/stable/3866c3553be8b268c8a7f8c0482c0c0177aa8bfa/VSCodeUserSetup-x64-1.59.1.exe"
     $FileName = "VSCodeUserSetup-x64-1.59.1.exe"
-    if ( -Not (Test-Path "$BasePath/Installer/Burp/$FileName")) {
-        Invoke-WebRequest -Uri $urlVSCode -OutFile "$BasePath/Installer/Burp/$FileName"
+    if ( -Not (Test-Path "$BasePath/Installer/vscode/$FileName")) {
+        Invoke-WebRequest -Uri $urlVSCode -OutFile "$BasePath/Installer/vscode/$FileName"
     }
 }
 function InstallPackages {
@@ -62,6 +70,7 @@ function InstallPackages {
     ) 
 }
 
+InitSetup
 DownloadProcMon
 DownloadWireshark
 DownloadMozilla
