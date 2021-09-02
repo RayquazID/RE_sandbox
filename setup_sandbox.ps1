099226eca2829f0ccf7e5e96ce05bc8ffb914895
rayquazid <rayquazid@protonmail.com>
2021-09-02 14:17:06 +0200

    added procmon,ws,code,burp, mozilla download

--- setup_sandbox.ps1
+++ setup_sandbox.ps1
@@ -26,10 +26,7 @@ function DownloadWireshark {
     $FileName = "Wireshark-win64-3.4.8.exe"
     if ( -Not (Test-Path "$BasePath/Installer/Wireshark/$FileName")) {
         Invoke-WebRequest -Uri $urlWireShark -OutFile "$BasePath/Installer/Wireshark/$FileName"
-        Remove-Item $FileName
-    }
-    else {
-        return
+
     }
 }
 function DownloadMozilla {
@@ -40,25 +37,24 @@ function DownloadMozilla {
     $FileName = "Firefox Setup 91.0.2.msi"
     if ( -Not (Test-Path "$BasePath/Installer/Mozilla/$FileName")) {
         Invoke-WebRequest -Uri $urlMozilla -OutFile "$BasePath/Installer/Mozilla/$FileName"
-        Remove-Item $FileName
-    }
-    else {
-        return
     }
 }
 function DownloadBurp {
     param (
     )
-    $urlProcMon = ""
-    $FileName = ""
-    if ( -Not (Test-Path $FileName)) {
-        Invoke-WebRequest -Uri $urlProcMon -OutFile $FileName
-        Expand-Archive $FileName -DestinationPath "$BasePath/Tools/ProcMon/" -Force
-        Remove-Item $FileName
+    $urlBurp = "https://portswigger.net/burp/releases/download?product=community&version=2021.8.2&type=WindowsX64"
+    $FileName = "burpsuite_community_windows-x64_v2021_8_2.exe"
+    if ( -Not (Test-Path "$BasePath/Installer/Mozilla/$FileName")) {
+        Invoke-WebRequest -Uri $urlBurp -OutFile "$BasePath/Installer/Burp/$FileName"
     }
-    else {
-        Expand-Archive $FileName -DestinationPath "$BasePath/Tools/ProcMon/" -Force
-        Remove-Item $FileName
+}
+function DownloadVSCode {
+    param (
+    )
+    $urlVSCode = "https://az764295.vo.msecnd.net/stable/3866c3553be8b268c8a7f8c0482c0c0177aa8bfa/VSCodeUserSetup-x64-1.59.1.exe"
+    $FileName = "VSCodeUserSetup-x64-1.59.1.exe"
+    if ( -Not (Test-Path "$BasePath/Installer/Burp/$FileName")) {
+        Invoke-WebRequest -Uri $urlVSCode -OutFile "$BasePath/Installer/Burp/$FileName"
     }
 }
 function InstallPackages {
@@ -67,3 +63,7 @@ function InstallPackages {
 }
 
 DownloadProcMon
+DownloadWireshark
+DownloadMozilla
+DownloadBurp
+DownloadVSCode
